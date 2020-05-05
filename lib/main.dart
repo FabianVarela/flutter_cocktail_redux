c95a5b79a8b598178acff276a05d1049b36a637b
FabianVarela <jfabian.303@gmail.com>
2020-05-04 19:30:53 -0500

    Initialize Redux: Persistor, Store and Epics

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_cocktail_redux/epics/cocktail.epic.dart';
+import 'package:flutter_cocktail_redux/models/app_state.dart';
+import 'package:flutter_cocktail_redux/reducers/main.reducer.dart';
+import 'package:flutter_cocktail_redux/views/cocktail_list.ui.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_epics/redux_epics.dart';
+import 'package:redux_persist/redux_persist.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  await SystemChrome.setPreferredOrientations(
+      <DeviceOrientation>[DeviceOrientation.portraitUp]);
+
+  await DotEnv().load('.env');
+
+  final Persistor<AppState> persist = Persistor<AppState>(
+    debug: true,
+    storage: FlutterStorage(location: FlutterSaveLocation.documentFile),
+    serializer: JsonSerializer<AppState>(AppState.fromJson),
+  );
+
+  final Epic<AppState> epics = combineEpics<AppState>(
+    <Epic<AppState>>[serviceCocktailEpic],
+  );
+
+  final EpicMiddleware<AppState> epicMiddleware =
+      EpicMiddleware<AppState>(epics);
+
+  dynamic initialState;
+
+  try {
+    initialState = await persist.load();
+  } catch (e) {
+    initialState = null;
   }
+
+  final Store<AppState> store = Store<AppState>(
+    appReducer,
+    initialState: initialState ?? AppState.initialState(),
+    middleware: <Middleware<AppState>>[
+      persist.createMiddleware(),
+      epicMiddleware,
+    ],
+  );
+
+  runApp(MyApp(store: store));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyApp extends StatefulWidget {
+  final Store<AppState> store;
 
-  final String title;
+  MyApp({Key key, this.store}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+    return StoreProvider<AppState>(
+      store: widget.store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: CocktailListUI(),
       ),
     );
   }
