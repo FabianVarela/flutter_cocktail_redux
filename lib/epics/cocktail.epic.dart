8bf295252e5634daed33e69d10311ded4ff0c59f
FabianVarela <jfabian.303@gmail.com>
2020-05-04 19:17:50 -0500

    Create epic to get cocktail categories from API client

---
+++ lib/epics/cocktail.epic.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter_cocktail_redux/actions/alert.action.dart';
+import 'package:flutter_cocktail_redux/actions/base.action.dart';
+import 'package:flutter_cocktail_redux/actions/cocktail.action.dart';
+import 'package:flutter_cocktail_redux/api/api_client.dart';
+import 'package:flutter_cocktail_redux/models/alert.model.dart';
+import 'package:flutter_cocktail_redux/models/app_state.dart';
+import 'package:flutter_cocktail_redux/models/cocktail.model.dart';
+import 'package:http/http.dart';
+import 'package:redux_epics/redux_epics.dart';
+
+Stream<dynamic> serviceCocktailEpic(
+    Stream<dynamic> actions, EpicStore<AppState> store) async* {
+  final ApiClient apiClient = ApiClient();
+
+  await for (final dynamic action in actions) {
+    switch (action.runtimeType) {
+      case GetCategoriesAction:
+        yield await getCategoryList(apiClient);
+        break;
+    }
+  }
+}
+
+Future<BaseAction> getCategoryList(ApiClient api) async {
+  try {
+    final Response response = await api.getCategoryList();
+    final dynamic decodedResponse = jsonDecode(response.body);
+    final List<CocktailCategory> categories = <CocktailCategory>[];
+
+    if (decodedResponse['drinks'] is List<dynamic>) {
+      for (final dynamic item in decodedResponse['drinks'])
+        categories.add(CocktailCategory.fromJson(item));
+    }
+
+    return CocktailCategoriesAction(categories: categories);
+  } catch (_) {
+    return AlertErrorAction(Alert(
+      type: 'error',
+      title: 'An error has ocurred',
+      message: 'Try again later',
+    ));
+  }
+}
