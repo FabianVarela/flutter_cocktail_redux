6f479f5209dbfa52788783408c9eaa78ed66a3eb
FabianVarela <jfabian.303@gmail.com>
2020-05-18 20:48:42 -0500

    Create function to hide alert

--- lib/view_model/alert.viewmodel.dart
+++ lib/view_model/alert.viewmodel.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_cocktail_redux/actions/alert.action.dart';
+import 'package:flutter_cocktail_redux/models/alert.model.dart';
 import 'package:flutter_cocktail_redux/models/app_state.dart';
 import 'package:redux/redux.dart';
 
 class AlertViewModel {
   final bool isLoading;
+  final Alert alert;
+  final Function() hideAlert;
 
-  AlertViewModel({@required this.isLoading});
+  AlertViewModel({
+    @required this.isLoading,
+    @required this.alert,
+    this.hideAlert,
+  });
 
   static AlertViewModel fromStore(Store<AppState> store) {
     return AlertViewModel(
       isLoading: store.state.isLoading,
+      alert: store.state.alert,
+      hideAlert: () => store.dispatch(AlertGenericAction(Alert())),
     );
   }
 
@@ -18,7 +28,8 @@ class AlertViewModel {
       identical(this, other) ||
       other is AlertViewModel &&
           runtimeType == other.runtimeType &&
-          isLoading == other.isLoading;
+          isLoading == other.isLoading &&
+          alert == other.alert;
 
   @override
   int get hashCode => 0;
